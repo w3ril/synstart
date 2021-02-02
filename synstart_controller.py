170cbdbb29544f8ea8c940ecf9a16cd9be68315d
w3ril <46866804+w3ril@users.noreply.github.com>
2021-02-02 19:50:44 +0300

    default start time, controller command
    - default start time - now + 5 seconds (scheduled_time)
    - scheduled command in controller request (daemon_starting_process)
    - specified daemon response timeout (daemon_response_timeout)

--- synstart_controller.py
+++ synstart_controller.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# ver 1.1 by Petr V. Redkin
+# ver 1.2 by Petr V. Redkin
 
 import datetime
 import socket
+import json
 from multiprocessing.dummy import Pool as ThreadPool
 
 log_fname = "synstart.log"
-daemon_ip = [ '192.168.1.28', '192.168.1.29', '192.168.4.204' ]
+daemon_ip = [ '192.168.1.28', '192.168.4.204', '192.168.1.179' ]
 daemon_port = 5555
+daemon_starting_process = "ls -ltr".split()
+daemon_response_timeout = 30
 thread_count = len(daemon_ip)
 pool = ThreadPool(thread_count) # thread count based on daemon ip count
-command = b"start_process_in_time 2021-02-02 11:18:00"
-#command = b"ntpupdate 194.190.168.1"
-#command = b"queue_status"
+scheduled_time = datetime.datetime.now() + datetime.timedelta(seconds=5) # start process on daemons in 5 seconds
+daemon_command = { "command": "start_process_in_time", "scheduled_start_time": "{}".format(scheduled_time), "daemon_starting_process": daemon_starting_process}
+#daemon_command = { "command": "start_process_in_time", "scheduled_start_time": "2021-02-02 16:11:00", "daemon_starting_process": daemon_starting_process}
+#daemon_command = { "command": "ntpupdate", "ip": "194.190.168.1"}
+#daemon_command = { "command": "queue_status" }
+
 
 def write_to_file_with_new_line_and_dt(text, fname):
     dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # for log
@@ -25,10 +31,12 @@ def write_to_file_with_new_line_and_dt(text, fname):
 
 def send_request(ip):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.settimeout(daemon_response_timeout)
 	write_to_file_with_new_line_and_dt("connecting to daemon_ip {}".format(ip), log_fname)
 	connect=sock.connect((ip,daemon_port))
-	write_to_file_with_new_line_and_dt("sending command {} to daemon".format(command), log_fname)
-	sock.send(command)
+	write_to_file_with_new_line_and_dt("sending daemon_command {} to daemon".format(daemon_command), log_fname)
+	j = json.dumps(daemon_command)
+	sock.send(j.encode())
 	write_to_file_with_new_line_and_dt("waiting ACK from daemon", log_fname)
 	data, addr = sock.recvfrom(4096)
 	if data == b"OK":
--- synstart_daemon.py
+++ synstart_daemon.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# ver 1.0 by Petr V. Redkin
+# ver 1.2 by Petr V. Redkin
 
 import datetime
 import time
 import socket
 import subprocess
+import json
 
 log_fname = "synstart.log"
 daemon_ip = '0.0.0.0'
 daemon_port = 5555
 controller_ip = ['192.168.1.28', '192.168.1.29', '192.168.1.179']
 sleep_time = 1/1000 # 1 msec (1/1000 of second) default; change for more precision but higher CPU load
-starting_process_cmd = "date"
+
 
 def write_to_file_with_new_line_and_dt(text, fname):
     dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # for log
@@ -25,7 +26,7 @@ def write_to_file_with_new_line_and_dt(text, fname):
 def convert_str_time_to_int(str_time):
 	return int(str_time.replace(" ", "").replace(":", "").replace("-", "").replace("-", "").replace(".", ""))
 
-def start_process_in_time(stime):
+def start_process_in_time(stime, daemon_starting_process):
 	write_to_file_with_new_line_and_dt("starting start_process_in_time: stime {}".format(stime), log_fname)
 	ln = len(stime)
 	stime_to_compare = convert_str_time_to_int(stime)
@@ -33,7 +34,7 @@ def start_process_in_time(stime):
 		actual_time = str(datetime.datetime.now())
 		actual_time_to_compare = convert_str_time_to_int(actual_time[0:ln])
 		if actual_time_to_compare >= stime_to_compare:
-			res = subprocess.run(starting_process_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+			res = subprocess.run(daemon_starting_process, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
 			write_to_file_with_new_line_and_dt("finished start_process_in_time: stime {}, stime_to_compare {}, stime length {}, actual stime {}, actual actual_time_to_compare {} result {}".format(stime, stime_to_compare, ln, actual_time, actual_time_to_compare, res), log_fname)
 			return
 		time.sleep(sleep_time)
@@ -46,6 +47,7 @@ def check_start_time(stime):
 	actual_time_to_compare = convert_str_time_to_int(actual_time[0:ln])
 	if actual_time_to_compare >= stime_to_compare:
 		res = False
+		write_to_file_with_new_line_and_dt("error check_start_time: scheduled starting time in the past!", log_fname)
 	else:
 		res = True
 	write_to_file_with_new_line_and_dt("finished check_start_time: stime {}, stime_to_compare {}, stime length {}, actual stime {}, actual actual_time_to_compare {} result {}".format(stime, stime_to_compare, ln, actual_time, actual_time_to_compare, res), log_fname)
@@ -68,31 +70,42 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind ((daemon_ip,daemon_port))
 while True:
 	data, addr = sock.recvfrom(4096)
-	# 1 - basic auth
+	# 1 - basic auth & request check
 	if addr[0] in controller_ip:
 		write_to_file_with_new_line_and_dt("udp data received from trusted controller ip/port: {}; data: {}".format(addr, data), log_fname)
+		if b"command" not in data:
+			write_to_file_with_new_line_and_dt("no commands in data; skip request".format(addr, data), log_fname)
+			sock.sendto(b"FAIL", addr)
+			continue
+		j = json.loads(data)
 	else:
 		write_to_file_with_new_line_and_dt("udp data received from untrusted controller ip/port: {}; data: {}".format(addr, data), log_fname)
+		sock.sendto(b"FAIL", addr)
 		continue
 	# 2 - get queue status
-	if data.startswith(b"queue_status"):
+	if j["command"] == "queue_status":
+		write_to_file_with_new_line_and_dt("queue_status ok", log_fname)
 		sock.sendto(b"OK", addr)
 	# 3 - ntpdate based on ntp (ip/fqdn) address from controller
-	elif data.startswith(b"ntpupdate"):
+	elif j["command"] == "ntpupdate":
 		data = data.decode()
-		ntp_server = data.split()[1]
+		ntp_server = j["ip"]
 		res = ntpupdate(ntp_server)
 		if res:
 			sock.sendto(b"OK", addr)
 		else:
 			sock.sendto(b"FAIL", addr)
 	# 4 - process starting
-	elif data.startswith(b"start_process_in_time"):
+	elif j["command"] == "start_process_in_time":
 		data = data.decode()
 		stime_list = data.split()[1:]
-		stime = " ".join(stime_list)
+		stime = j["scheduled_start_time"]
+		daemon_starting_process = j["daemon_starting_process"]
 		if check_start_time(stime):
 			sock.sendto(b"OK", addr)
-			start_process_in_time(stime)
+			start_process_in_time(stime, daemon_starting_process)
 		else:
 			sock.sendto(b"FAIL", addr)
+	else:
+		write_to_file_with_new_line_and_dt("unknown command; data {}".format(data), log_fname)
+		sock.sendto(b"FAIL", addr)

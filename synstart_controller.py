02631dece8f43954c5b6e4a8c1f199e25778d6d1
w3ril <46866804+w3ril@users.noreply.github.com>
2021-02-02 11:20:12 +0300

    added threading

--- synstart_controller.py
+++ synstart_controller.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# ver 1.0 by Petr V. Redkin
+# ver 1.1 by Petr V. Redkin
 
 import datetime
 import socket
+from multiprocessing.dummy import Pool as ThreadPool
 
 log_fname = "synstart.log"
 daemon_ip = [ '192.168.1.28', '192.168.1.29', '192.168.4.204' ]
 daemon_port = 5555
-#command = b"start_process_in_time 2021-02-01 19:58:00"
+thread_count = len(daemon_ip)
+pool = ThreadPool(thread_count) # thread count based on daemon ip count
+command = b"start_process_in_time 2021-02-02 11:18:00"
 #command = b"ntpupdate 194.190.168.1"
 #command = b"queue_status"
 
@@ -20,9 +23,7 @@ def write_to_file_with_new_line_and_dt(text, fname):
         print(t)
         the_file.write(t + '\n')
 
-
-write_to_file_with_new_line_and_dt("syn_start controller started", log_fname)
-for ip in daemon_ip:
+def send_request(ip):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	write_to_file_with_new_line_and_dt("connecting to daemon_ip {}".format(ip), log_fname)
 	connect=sock.connect((ip,daemon_port))
@@ -37,3 +38,7 @@ for ip in daemon_ip:
 		raise Exception("unsuccessful communication with daemon")
 	write_to_file_with_new_line_and_dt("closing socket with daemon", log_fname)
 	sock.close()
+
+
+write_to_file_with_new_line_and_dt("syn_start controller started", log_fname)
+pool.map(send_request, daemon_ip)
